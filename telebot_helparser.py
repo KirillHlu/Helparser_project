ef86b8f525308fbab450cc49be64f45be8994b8f
KirillHlu <164990613+KirillHlu@users.noreply.github.com>
2024-04-14 13:09:10 +0700

    Update telebot_helparser.py

--- telebot_helparser.py
+++ telebot_helparser.py
@@ -1,24 +1,37 @@
 import telebot
 from telebot import types
 import requests
+from bs4 import BeautifulSoup
+import time
 
-city = ""
+city = "New York City"
+
 
 bot = telebot.TeleBot("TOKEN")
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     markup = types.ReplyKeyboardMarkup()
+    btn1 = types.KeyboardButton('/commands')
     btn3 = types.KeyboardButton('/weather')
-    markup.row(btn3)
-    bot.send_message(message.chat.id, 'Hello, it is helparser, send /set_city to save your city.', reply_markup=markup)
+    markup.row(btn1, btn3)
+    bot.send_message(message.chat.id, 'Hello, it is helparser, send /commands to see commands.', reply_markup=markup)
+
+@bot.message_handler(commands=['commands'])
+def start_message(message):
+    bot.send_message(message.chat.id, '/set_city - set the city to view the weather in it (/set_city "city"). \n\n/weather - view the weather.\n\n/search_by_word - command for parsing a page by a specific word (/search_by_word link word)\n\n/search_by_inf - find the text according to the given information (/search_by_inf url class tag)')
 
 @bot.message_handler(commands=['set_city'])
 def set_city(message):
-    global city
-    city = message.text
-    city = city.replace('/set_city ', '')
-    bot.reply_to(message, f"Your city ({city}) has been saved, enter /weather to view the weather")
+    try:
+        global city
+        city = message.text
+        city = city.split()
+        city = city[1]
+        bot.reply_to(message, f"Your city ({city}) has been saved, enter /weather to view the weather")
+
+    except IndexError:
+        bot.reply_to(message, f'Please, send your city correctly!')
 
 @bot.message_handler(commands=['weather'])
 def get_weather(message):
@@ -29,6 +42,51 @@ def get_weather(message):
         bot.send_photo(message.chat.id,'https://get.wallhere.com/photo/2560x1440-px-abstract-geometry-lake-landscape-minimalism-mountains-Sun-trees-1425123.jpg',caption=caption)
     else:
         bot.send_message(message.chat.id, "Not found.")
+
+@bot.message_handler(commands=['search_by_word'])
+def search_by_word(message):
+    try:
+        word_and_link_for_search = message.text
+        word_and_link_for_search = word_and_link_for_search.split()
+        url = word_and_link_for_search[1]
+        word = word_and_link_for_search[2]
+
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        word = word.lower()
+        elements = soup.find_all(text=lambda text: word in text.lower())
+
+        for element in elements:
+            parent = element.parent
+            if parent.name != 'script':
+                bot.send_message(message.chat.id, f'Tag: {parent.name}\nClass: {parent.get("class")}\n Text: {element.strip()}\n')
+                time.sleep(0.8)
+
+    except IndexError:
+        bot.reply_to(message, f'Please, send your information correctly!')
+
+@bot.message_handler(commands=['search_by_inf'])
+def search_by_inf(message):
+    try:
+        inf = message.text
+        inf = inf.split()
+        url = inf[1]
+        class1 = inf[2]
+        tag = inf[3]
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        data = soup.findAll(tag, class_=class1)
+
+        bot.send_message(message.chat.id, f'The information from {url} ({tag}, {class1}): ')
+        for i in data:
+            bot.send_message(message.chat.id, f'\n{i.text}')
+            time.sleep(0.8)
+
+
+    except IndexError:
+        bot.reply_to(message, f'Please, send your information correctly!')
+
 
 @bot.message_handler(content_types=['text'])
 def talk(message):
