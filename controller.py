929f817f7da642662b3ef30609b99007dab8401f
KirillHlu <164990613+KirillHlu@users.noreply.github.com>
2024-04-21 22:02:52 +0700

    Update controller.py

--- controller.py
+++ controller.py
@@ -3,7 +3,7 @@ from tkinter import *
 from tkinter import ttk
 from PIL import Image, ImageTk, ImageDraw, ImageFont
 from translation import translate1
-from model import Parsing1
+from model import Parsing1, Tg_bot
 import datetime
 
 class Main_root(Tk):
@@ -34,12 +34,15 @@ class Main_root(Tk):
         self.btn5 = ttk.Button(text=translate1['Settings'], padding=10, width=20, command=SettingsApp)
         self.btn6 = ttk.Button(text=translate1['Qr-Code'], padding=10, width=20, command=QRCodeGenerator)
         self.btn7 = ttk.Button(text=translate1['Working with a parsed files'], command=Txt_root, padding=10, width=20)
+        self.btn8 = ttk.Button(text=translate1['Telegram bot'], command=Tg_bot().open_link, padding=8, width=25)
 
 
         news_list = Parsing1().news()
 
         # Update the news1 Text widget with the news and time lists
         if translate1['Your country has no news'] in news_list:
+            self.news1.insert(END, f'Try our tg bot!\n')
+            self.news1.insert(END, f'https://t.me/helparser_bot\n\n\n')
             self.news1.insert(END, f'\n{news_list} \n\n\n')
 
         else:
@@ -48,9 +51,7 @@ class Main_root(Tk):
 
         self.news1.config(state="disabled")
 
-
         weather_info = Parsing1().get_weather()
-
 
         image = Image.open("weather1.jpg")
         draw = ImageDraw.Draw(image)
@@ -67,7 +68,6 @@ class Main_root(Tk):
         label = Label(self, image=photo)
         label.image = photo
 
-
         image2 = Image.open("Main_qr.png")
         image2 = image2.resize((80, 80))
         photo2 = ImageTk.PhotoImage(image2)
@@ -81,6 +81,7 @@ class Main_root(Tk):
         self.btn5.place(x=500, y=100)
         self.btn6.place(x=400, y=260)
         self.btn7.place(x=600, y=260)
+        self.btn8.place(x=485, y=330)
         label.place(x=680, y=00)
         self.label2.place(x=718, y=370)
 
